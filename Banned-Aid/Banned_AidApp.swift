e483b0320345c11bba6da5a87fbb3e38749293ed
anya <anyakondamani@gmail.com>
2023-07-19 10:54:37 -0400

    Initial Commit

---
+++ Banned-Aid/Banned_AidApp.swift
@@ -0,0 +1,17 @@
+//
+//  Banned_AidApp.swift
+//  Banned-Aid
+//
+//  Created by anya kondamani on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Banned_AidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
